bb99627a8f99890c2c2ef2848c6a31d39d29f8a2
Tersoo <codesage@live.com>
2024-10-24 19:19:34 +0100

    feat: implement add, drop, sync features on control object

--- tests/test_control.py
+++ tests/test_control.py
@@ -1,7 +1,9 @@
+import os
+from pathlib import Path
 from unittest import TestCase
 
 from pidentity import Contract, Control
-from pidentity.constants import CONTACT, CONTENT, CONTEXT
+from pidentity.constants import CONTACT, CONTENT, CONTEXT, ON, TO, AT
 
 
 DB = 'pidentity'
@@ -10,6 +12,7 @@ DB = 'pidentity'
 class ControlTest(TestCase):
     def setUp(self) -> None:
         self.control = Control(DB)
+        self.control.inits()
         return super().setUp()
 
     def test_control_payload_structure(self):
@@ -25,33 +28,45 @@ class ControlTest(TestCase):
         contract.on('post').to('/customers/:id')
         control.add(contract)
         self.assertEqual(control._contracts[0], contract._payload)
+
+        saved = control.saved
         for i, v in enumerate([CONTACT, CONTENT, CONTEXT]):
-            self.assertDictEqual(control._unsaved[i], {
-                'activity': 'post',
-                'towards': '/customers/:id',
-                'regulates': v,
+            self.assertDictEqual(saved[i], {
+                ON: 'post',
+                TO: '/customers/:id',
+                AT: v,
                 'conditions': '{}'
             })
+        self.assertListEqual([], control.saved)
+
+        cursor = control.cursor
+        one = cursor.execute('select * from regulators').fetchone()
+        cursor.close()
+        self.assertIsNotNone(one)
+
+    def test_control_drop(self):
+        _sql = f"select * from regulators where {TO} = '/orders/:id/line-items'"
+        control = Control(engine=DB)
+        contract = Contract()
+        control.add(
+        contract.upon('post', 'get', 'put', 'delete', 'patch').unto('/orders/:id/line-items'))
+        cursor = control.cursor.execute(_sql)
+        rows = cursor.fetchall()
+        cursor.close()
+        self.assertEqual(len(rows), 3)
+
+        control.drop(contract)
+        cursor = control.cursor.execute(_sql)
+        rows = cursor.fetchall()
+        cursor.close()
+        self.assertEqual(len(rows), 0)
+
 
     def test_control_raises_when_contract_not_activated(self):
         # every contract must call contract.on.to before being added to controls
         control = Control(engine=DB)
         contract = Contract(domain='mydomain')
         self.assertRaises(ValueError, control.add, contract)
-
-    def test_control_save(self):
-        control = Control(engine=DB)
-        contract = Contract(domain='mydomain')
-        contract.on('post').to('/customers/:id')
-        control.add(contract)
-        control.save()
-
-    def test_control_drop(self):
-        control = Control(engine=DB)
-        contract = Contract(domain='mydomain')
-        contract.on('post').to('/customers/:id') 
-        control.drop(contract)
-
 
     def test_control_content_evaluation(self):
         control = Control(engine=DB)
@@ -72,3 +87,16 @@ class ControlTest(TestCase):
             'owner': 10
         })
         self.assertTrue(guard.scans())
+    
+    def test_control_sync(self):
+        self.assertTrue(self.control.sync('test'))
+        self.assertTrue(Path('.pidentity/test.json').exists())
+        self.assertTrue(Path('.pidentity/test.db').exists())
+
+        cursor = self.control.cursor
+        rows = cursor.execute("select * from regulators where unto = 'uu7483-eea3a-akdje283-adkea'").fetchall()
+        cursor.close()
+        self.assertEqual(len(rows), 1)
+    
+    def test_control_swap(self):
+        raise ValueError
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,7 +1,1 @@
 from pidentity import Contract, Control
-
-
-contract = Contract(domain='mydomain')
-control = Control(engine='myengine')
-
-contract.on('post').to('/customers/:id')
